1dfbcfe0437dcaab07c0b6a16132030983bf1808
JereUR <jeremias.jdv@gmail.com>
2023-10-29 20:44:56 -0300

    feat(Root): Toaster component from sonner dependencie added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from "sonner";
+
 import "./App.css";
 import { CreateNewUser } from "./components/CreateNewUser";
 import ListOfUsers from "./components/ListOfUsers";
@@ -7,6 +9,7 @@ function App() {
 		<>
 			<ListOfUsers />
 			<CreateNewUser />
+			<Toaster richColors />
 		</>
 	);
 }
